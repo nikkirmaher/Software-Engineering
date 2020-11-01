d21fb7363e30cb3afd56e4a4393a70b315421181
AustinRD <austinrd95@gmail.com>
2020-11-01 16:59:37 -0500

    enabled editing and support for the new db schema

--- components/search/buildingSearch.php
+++ components/search/buildingSearch.php
@@ -1,59 +1,134 @@
+<?php
+	//Making the database connection
+	include_once("./backend/db_connector.php");
+
+	//Edit Section 
+	//If submit button is pressed.
+	if (isset($_POST['editBuilding'])) {	
+		$bID = $_POST['edit-bid'];
+		$building_name = $_POST['edit-building-name'];
+		$building_abbreviation = $_POST['edit-building-abbreviation'];
+		
+		//SQL Update Statement for buildingSearch form
+		$sql = "UPDATE `buildings`
+			SET `building_name` = '$building_name', 
+				`building_abbreviation` = '$building_abbreviation'
+			WHERE BID=$bID";
+		
+		if ($dbconn->query($sql) === TRUE) {
+			echo "Building edited successfully";
+		}
+		else {
+			echo "Error: " . $sql . "<br>" . $dbconn->error;
+		}
+	}
+?>
+
 <div id="buildingSearch">
 	<h2>Search Building</h2>
-	<form name="search-building" method="get" action="...">
-		<input type="text" placeholder="Enter Building Here.." name="search-building">
-		<button id="submitBuilding" type="submit"><i class="fa fa-search"></i></button>
-	</form>
+	
+	<input id="userInput" type="text" placeholder="Enter Building Here.." name="search-building">
+	<button id="searchBuilding" type="button">Search</button>
+	<br><br>
 
-	<table>
+	<!-- Building Table - lists all buildings from the database. -->
+	<table id="buildingTable">
 		<tr>
 			<th>BID</th>
 			<th>Building Name</th>
 			<th>Building Abbreviation</th>
 			<th>Action</th>
-		</tr>
-		
+		</tr>		
 		<?php 
-			include_once("./backend/db_connector.php");
-
 			$sql = "SELECT * FROM `buildings`";
 			$result = mysqli_query($dbconn, $sql);
-			while ($row = mysqli_fetch_assoc($result)) {
-				$bid = $row['BID'];
-				$bname = $row['building_name'];
-				$babrv = $row['building_abbreviation'];
+			$numResults = mysqli_num_rows($result);
+
+			if($numResults < 1) {
+                echo("<tr>
+                    <td colspan='10'>No Buildings found</td>
+                    </tr>");
+            }
+            else {
+				$rowNum = 0;
+				while ($row = mysqli_fetch_assoc($result)) {
+					$bid = $row['BID'];
+					$bname = $row['building_name'];
+					$babrv = $row['building_abbreviation'];
 		?>
 		<tr>
 			<td><?php echo $bid; ?></td>
 			<td><?php echo $bname; ?></td>
 			<td><?php echo $babrv; ?></td>
-			<td>Edit</td>
+			<td><button type="button" onclick="displayModal(<?php echo ++$rowNum ?>, 'buildingTable')">View</button></td>
 		</tr>
-		<?php } ?>
+		<?php 
+				}
+			}
+		?>
 	</table>
 </div>
-		<?php 
-		//Edit Section 
-        //If submit button is pressed.
-        if (isset($_GET['editBuilding'])) 
-        {
-            include_once("./backend/db_connector.php");
-			
-			$bID = $_GET['edit_building'];
-            $building_name = $_GET['update-building-name'];
-            $building_abbreviation = $_GET['update-building-abbreviation'];
-		   
-			//SQL Update Statement for buildingSearch form
-			$sql = "UPDATE `buildings` 
-                SET building_name = '$building_name', 
-					building_abbreviation = '$building_abbreviation';
-				WHERE id=$bID";
 
-            if ($dbconn->query($sql) === TRUE) {
-                echo "New record edited successfully";
-              } else {
-                echo "Error: " . $sql . "<br>" . $dbconn->error;
-              }
-              $dbconn->close();
-        }
-        ?>
+<!-- The view modal for a selected building, it uses the same form-layout 
+    for creating buildings and populates its fields with data from directly
+    from the table instead of making another database call. -->
+<div id="myModal" class="modal">
+	<div class="modal-content">
+		<span class="close">&times;</span>
+		<p class="title">Edit Building</p>
+		<form name="edit-building" method="post" action="./search.php?searchType=building">
+			<input type="hidden" name="edit-bid" id="edit-bid"> 
+
+			<label for="edit-building-name"> Building Name: </label>
+			<input type="text" name="edit-building-name" id="edit-building-name" onkeyup="enableButton();">
+			<br>
+			
+			<label for="edit-building-abbreviation"> Building Abbreviation: </label>
+			<input type="text" name="edit-building-abbreviation" id="edit-building-abbreviation" onkeyup="enableButton();">
+			<br>
+
+			<button type="submit" name="editBuilding" id="editBuilding" disabled>Save Changes</button>
+            <button type="button" name="cancel" onclick="document.getElementById('myModal').style.display = 'none';">Cancel</button>
+		</form>
+	</div>
+</div>
+
+<script>
+	//Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal and populate the fields
+	// with information from the selected table row.
+	function displayModal(tableRow, tableID) {
+		var editRow = document.getElementById(tableID).rows[tableRow];
+		modal.style.display = "block";
+
+		//Start populating the fields.
+		document.getElementById('edit-bid').value = editRow.cells[0].innerText;
+		document.getElementById('edit-building-name').value = editRow.cells[1].innerText; 
+		document.getElementById('edit-building-abbreviation').value = editRow.cells[2].innerText;
+	}
+
+	// Only give the user the option to save changes if changes are made.
+    function enableButton() {
+        document.getElementById('editBuilding').disabled = false;
+	}
+	
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+</script>
--- components/search/roomSearch.php
+++ components/search/roomSearch.php
@@ -1,13 +1,47 @@
+<?php
+	//Making the database connection
+	include_once("./backend/db_connector.php");
+
+	//Edit Section
+	//If submit button is pressed.
+	if (isset($_POST['editRoom'])) {		
+		$rID = $_GET['editRoom'];
+		$building = $_GET['select-building'];
+		$room_num = $_GET['room-number'];
+		$short_name = $_GET['room-short-name'];
+		$is_required = $_GET['is_req'];
+		$max_seats = $_GET['Max_seats'];
+		
+		//SQL Update Statement for roomSearch form
+		$sql = "UPDATE `room` 
+			SET building = '$building', 
+				room_num = '$room_num', 
+				short_name = '$short_name', 
+				is_required = '$is_required', 
+				max_seats = '$max_seats';
+			WHERE id=$rID";
+
+		if ($dbconn->query($sql) === TRUE) {
+			echo "Room edited successfully";
+		}
+		else {
+			echo "Error: " . $sql . "<br>" . $dbconn->error;
+		}
+	}
+?>
+
 <div id="roomSearch">
 	<h2>Search Room</h2>
-	<form name="search-room" method="get" action="...">
-		<input type="text" placeholder="Enter Room Here.." name="search-room">
-		<button id="submitRoom" type="submit"><i class="fa fa-search"></i></button>
-	</form>
 
-	<table>
+	<input id="userInput" type="text" placeholder="Enter Room Here.." name="search-room">
+	<button id="searchRoom" type="button">Search</button>
+	<br><br>
+
+	<!-- Room table - lists all rooms from the database. -->
+	<table id="roomTable">
 	<tr>
 		<th>RID</th>
+		<th>BID</th>
 		<th>Room Number</th>
 		<th>Room Short Name</th> 
 		<th>Required?</th>
@@ -15,61 +49,148 @@
 		<th>Maximum Seats</th>
 		<th>Action</th>
 	</tr>
-	
 	<?php 
-		include_once("./backend/db_connector.php");
-
-		$sql = "SELECT * FROM `room`";
+		$sql = "SELECT * FROM `rooms`";
 		$result = mysqli_query($dbconn, $sql);
-		while ($row = mysqli_fetch_assoc($result)) {
-			$roomID = $row['RID'];
-			$roomNum = $row['room_num'];
-			$shortName = $row['short_name'];
-			$isRequired = $row['is_required'];
-			$isExclusive = $row['is_exclusive'];
-			$maxSeats = $row['max_seats'];
+		$numResults = mysqli_num_rows($result);
+
+		if($numResults < 1) {
+			echo("<tr>
+				<td colspan='10'>No Rooms found</td>
+				</tr>");
+		}
+		else {
+			$rowNum = 0;
+			while ($row = mysqli_fetch_assoc($result)) {
+				$rid = $row['RID'];
+				$bid = $row['BID'];
+				$roomnum = $row['room_num'];
+				$roomshort = $row['short_name'];
+				$required = $row['is_required'];
+				$exclusive = $row['is_exclusive'];
+				$maxseats = $row['max_seats'];
 	?>
 	<tr>
-		<td><?php echo $roomID; ?></td>
-		<td><?php echo $roomNum; ?></td>
-		<td><?php echo $shortName; ?></td>
-		<td><?php echo $isRequired; ?></td>
-		<td><?php echo $isExclusive; ?></td>
-		<td><?php echo $maxSeats; ?></td>
-		<td>Edit</td>
+		<td><?php echo $rid; ?></td>
+		<td><?php echo $bid; ?></td>
+		<td><?php echo $roomnum; ?></td>
+		<td><?php echo $roomshort; ?></td>
+		<td><?php echo $required; ?></td>
+		<td><?php echo $exclusive; ?></td>
+		<td><?php echo $maxseats; ?></td>
+		<td><button type="button" onclick="displayModal(<?php echo ++$rowNum ?>, 'roomTable')">View</button></td>
 	</tr>
-	<?php } ?>
+	<?php 
+			}
+		}
+	?>
 	</table>
 </div>
-		<?php
-		//Edit Section
-        //If submit button is pressed.
-        if (isset($_GET['editRoom'])) 
-        {
-            include_once("./backend/db_connector.php");
-			
-			$rID = $_GET['editRoom'];
-            $building = $_GET['select-building'];
-            $room_num = $_GET['room-number'];
-            $short_name = $_GET['room-short-name'];
-            $is_required = $_GET['is_req'];
-            $max_seats = $_GET['Max_seats'];
-			
-			//SQL Update Statement for roomSearch form
-            $sql = "UPDATE `room` 
-                    SET building = '$building', 
-						room_num = '$room_num', 
-						short_name = '$short_name', 
-						is_required = '$is_required', 
-						max_seats = '$max_seats';
-					WHERE id=$rID";
 
+<!-- The view modal for a selected room, it uses the same form-layout 
+    for creating rooms and populates its fields with data from directly
+    from the table instead of making another database call. -->
+<div id="myModal" class="modal">
+	<div class="modal-content">
+		<span class="close">&times;</span>
+		<p class="title">Edit Room</p>
+		<form name="edit-building" method="post" action="./search.php?searchType=building">
+			<input type="hidden" name="edit-rid" id="edit-rid"> 
 
-            if ($dbconn->query($sql) === TRUE) {
-                echo "New record edited successfully";
-              } else {
-                echo "Error: " . $sql . "<br>" . $dbconn->error;
-              }
-              $dbconn->close();
-        }
-        ?>
+			<label for="edit-bid">Building:</label>
+			<input type="text" name="edit-bid" id="edit-bid" onkeyup="enableButton();">
+			<br>
+			<label for="edit-room-number">Room Number:</label>
+			<input type="text" name="edit-room-number" id="edit-room-number" onkeyup="enableButton();">
+			<br>
+			<label for="edit-short-name">Room Short Name:</label>
+			<input type="text" name="edit-short-name" id="edit-short-name" onkeyup="enableButton();">
+			<br>
+
+			Is this room Required by any courses?<br>
+			<input type="radio" name="edit-required" id="edit-required-yes" value="1" onchange="enableButton();">
+			<label for="edit-required">Yes</label>
+			<input type="radio" name="edit-required" id="edit-required-no" value="0" onchange="enableButton();">
+			<label for="edit-required">No</label>
+			<br>
+
+			Is this room Exclusively for any courses?<br>
+			<input type="radio" name="edit-exclusive" id="edit-exclusive-yes" value="1" onchange="enableButton();">
+			<label for="edit-exclusive-yes">Yes</label>
+			<input type="radio" name="edit-exclusive" id="edit-exclusive-no" value="0" onchange="enableButton();">
+			<label for="edit-exclusive-no">No</label>
+			<br>
+
+			
+			<label for="edit-max-seats">Max Seats:</label>
+			<input type="number" name="edit-max-seats" id="edit-max-seats" onchange="enableButton();">
+			<br>
+
+			<button type="submit" name="editRoom" id="editRoom" disabled>Save Changes</button>
+            <button type="reset" name="reset" onclick="document.getElementById('myModal').style.display = 'none';">Cancel</button>
+		</form>
+	</div>
+</div>
+
+<script>
+	//Get the modal
+	var modal = document.getElementById("myModal");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal and populate the fields
+	// with information from the selected table row.
+	function displayModal(tableRow, tableID) {
+		var editRow = document.getElementById(tableID).rows[tableRow];
+		modal.style.display = "block";
+
+		//Start populating the fields.
+		document.getElementById('edit-rid').value = editRow.cells[0].innerText;
+		document.getElementById('edit-bid').value = editRow.cells[1].innerText; 
+		document.getElementById('edit-room-number').value = editRow.cells[2].innerText;
+		document.getElementById('edit-short-name').value = editRow.cells[3].innerText;
+
+		//Required and Exclusive fields have boolean values and require
+		//user input to be either yes or no.
+		if(editRow.cells[4].innerText === "0") {
+			document.getElementById('edit-required-no').checked = true;
+			document.getElementById('edit-required-yes').value = false;
+		}
+		else if(editRow.cells[4].innerText === "1") {
+			document.getElementById('edit-required-yes').value = true;
+			document.getElementById('edit-required-no').checked = false;
+		}
+		
+		if(editRow.cells[5].innerText === "0") {
+			document.getElementById('edit-exclusive-no').checked = true;
+			document.getElementById('edit-exclusive-yes').value = false;
+		}
+		else if(editRow.cells[5].innerText === "1") {
+			document.getElementById('edit-exclusive-yes').value = true;
+			document.getElementById('edit-exclusive-no').checked = false;
+		}
+
+		document.getElementById('edit-max-seats').value = editRow.cells[6].innerText;
+	}
+
+	// Only give the user the option to save changes if changes are made.
+    function enableButton() {
+        document.getElementById('editRoom').disabled = false;
+	}
+	
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+</script>
