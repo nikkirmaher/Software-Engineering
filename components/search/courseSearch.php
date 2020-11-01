a0ff44edf7b98ca6b5df8eb167547646ec2f4337
Shawn Cimorelli <cimorelli.shawn@gmail.com>
2020-11-01 17:34:37 -0500

    changed form

--- components/search/courseSearch.php
+++ components/search/courseSearch.php
@@ -1,9 +1,9 @@
 <div id="courseSearch">
 	<h2>Search Course</h2>
-	<form name="search-course" method="get" action="...">
+	
 		<input type="text" placeholder="Enter Course Here.." name="search-course">
 		<button id="submitCourse" type="submit"><i class="fa fa-search"></i></button>
-	</form>
+
 
 	<table>
 		<tr>
